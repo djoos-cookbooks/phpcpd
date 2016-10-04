311a254e8a98b4a898f1cd45a128fb0fb1edce6e
Dieter Blomme <dieterblomme@gmail.com>
2016-10-04 18:06:16 +0200

    Use composer_install_global

--- attributes/composer.rb
+++ attributes/composer.rb
@@ -5,4 +5,7 @@
 # Copyright (c) 2016, David Joos
 #
 
-default['phpcpd']['install_dir'] = '/usr/local/phpcpd'
+default['phpcpd']['install_dir'] = default['composer']['global_install']['install_dir']
+if default['phpcpd']['install_method'] == 'composer'
+  default['phpcpd']['bin_dir'] = default['composer']['global_install']['bin_dir']
+end
